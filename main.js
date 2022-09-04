278b86835e98fcfbb20be80d901a03add5d8f69e
PRATHAMESH BAGADE <prathameshbagade02@gmail.com>
2022-09-04 18:46:40 +0530

    Creating interactive rating card

---
+++ main.js
@@ -0,0 +1,20 @@
+const mainContainer = document.querySelector(".main-container")
+const thanksContainer = document.querySelector(".thank-you");
+
+const submitButton = document.getElementById("submit"); 
+
+const rating = document.getElementById("rating");
+const rates = document.querySelectorAll(".btn");
+submitButton.addEventListener("click",() => {
+    thanksContainer.classList.remove("hidden");
+    mainContainer.style.display = "none";
+});
+
+rates.forEach((rate) => {
+    rate.addEventListener("click",() => {
+        rating.innerHTML = rate.innerHTML
+    })
+})
+
+
+
